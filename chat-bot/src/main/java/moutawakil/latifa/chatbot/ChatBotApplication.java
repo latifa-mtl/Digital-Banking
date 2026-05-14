8aa4c60695f398265f473dae9d0ca601db15381d
Latifa Moutawakil <latefamoutawakil@gmail.com>
2026-05-14 15:23:12 +0100

    chat-bot

---
+++ chat-bot/src/main/java/moutawakil/latifa/chatbot/ChatBotApplication.java
@@ -0,0 +1,13 @@
+package moutawakil.latifa.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotApplication.class, args);
+    }
+
+}
---
+++ chat-bot/src/test/java/moutawakil/latifa/chatbot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package moutawakil.latifa.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
